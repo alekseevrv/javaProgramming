8b55889e78ce17455344bcb0450d5d53de67b8d7
alekseevrv <alekseev.ruslan_@mail.ru>
2018-10-19 22:32:29 +0700

    Добавление модулей, библиотеки тестирования junit и библиотеки для  работы с Json. В common добавлен тест PointDTO.  В тесте добавлены два метода: преобразование полей объекта в Json и  преобразование Json в объект.

---
+++ common/src/test/java/jdev/dto/PointDTOTest.java
@@ -0,0 +1,42 @@
+package jdev.dto;
+
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by jdev on 06.03.2017.
+ */
+public class PointDTOTest {
+
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1539962123782}";
+    private String autoId = "o567gfd";
+
+    @Test // преобразование полей объекта в Json
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        assertTrue(point.toJson().contains("\"time\":"));
+        System.out.println(point.toJson());
+    }
+
+    @Test // преобразование Json в объект
+    public void decodeDto() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        assertEquals(autoId, dto.getAutoId());
+        assertEquals(1539962123782L, dto.getTime());
+        System.out.println(dto.getLat() + " " +
+                dto.getLon() + " " +
+                dto.getAutoId());
+    }
+}
