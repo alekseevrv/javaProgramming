f84b4baa50f1b0cff0392bab37024a1d1fc69c09
alekseevrv <alekseev.ruslan_@mail.ru>
2018-10-23 21:27:17 +0700

    cron.prop

--- properties/src/main/java/jdev/PropContext.java
+++ properties/src/main/java/jdev/PropContext.java
@@ -4,8 +4,10 @@ import jdev.services.InjectedService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 @PropertySource("classpath:/app.properties")
 public class PropContext {
 
