2e69979d73bf84660fd419a951464cd170e8c182
alekseevrv <alekseev.ruslan_@mail.ru>
2018-11-11 23:31:52 +0700

    use FortuneService

---
+++ mvc-boot/src/main/java/services/FortuneService.java
@@ -0,0 +1,11 @@
+package services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FortuneService {
+
+    public double tryFortune() {
+        return Math.random();
+    }
+}
