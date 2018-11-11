c9e47d2e3056b3f66c79e21571300089962f2520
alekseevrv <alekseev.ruslan_@mail.ru>
2018-11-11 23:57:42 +0700

    пересылка данных и получение ответа

--- mvc-boot/src/main/java/controllers/CounterController.java
+++ mvc-boot/src/main/java/controllers/CounterController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,14 @@ public class CounterController {
     @RequestMapping("/counter")
     public CurrentState greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new CurrentState(counter.incrementAndGet(),
-                            String.format(template, name));
+                String.format(template, name));
+    }
+
+    @RequestMapping("/coords")
+    public Response setCoords(@RequestParam(value="location") String location){
+        System.out.println(location);
+        Response response = new Response("ok", true);
+
+        return response;
     }
 }
